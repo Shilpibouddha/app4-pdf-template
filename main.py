adda1962c1345b4204e32f54e561284ca010f378
Shilpibouddha <152995175+Shilpibouddha@users.noreply.github.com>
2024-02-12 18:44:11 +0530

    Day 24:Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fpdf import FPDF #FPDF is a class which creates pdf instances
+import pandas as pd
+
+
+pdf=FPDF(orientation="P", unit="mm" , format="A4") #P stand for portrait L for landscape
+
+df=pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L",ln=1)#w=0 means till end of page, align =l means start from left side ,ln=1 means start from next line
+    pdf.line(10,21,200,21)
+
+
+pdf.output("output.pdf")
