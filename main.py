c9fd0531643baa955c89cce1603796618e47a306
Shilpibouddha <152995175+Shilpibouddha@users.noreply.github.com>
2024-02-19 09:18:57 +0530

    Day 24:Exercise add lines

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L",ln=1)#w=0 means till end of page, align =l means start from left side ,ln=1 means start from next line
-    pdf.line(10,21,200,21)
+    for y in range(20,298,10):
+
+        pdf.line(10,y,200,y)
+
 
 
 pdf.output("output.pdf")
